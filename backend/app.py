7cc7606085d80aa98de075f71cf8c90f26a64a25
mskRuvanthika <mskruvanthika2007@gmail.com>
2026-09-19 23:11:14 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -25,6 +25,7 @@ app.add_middleware(
         "http://localhost:5173",
         "http://127.0.0.1:5173",
         "https://mskruvanthika.github.io",
+        "https://farmvision-60088588626.development.catalystserverless.in",
     ],
     allow_credentials=True,
     allow_methods=["*"],
