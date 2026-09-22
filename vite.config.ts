94341525ab67a99618ea73681aa5c22b6cdc6762
mskRuvanthika <mskruvanthika2007@gmail.com>
2026-09-22 17:29:25 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: "/FarmVision/",
+  base: "/Lumina/",
   plugins: [react(), tailwindcss()],
 
   server: {
